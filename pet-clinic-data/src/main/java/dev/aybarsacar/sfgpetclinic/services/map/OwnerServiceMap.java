fcc88ec321552e14c825669dbe502577cf1bec41
Aybars Acar <aybarsacar@gmail.com>
2020-12-24 19:01:33 +1100

    index page added

---
+++ pet-clinic-data/src/main/java/dev/aybarsacar/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -0,0 +1,39 @@
+package dev.aybarsacar.sfgpetclinic.services.map;
+
+import dev.aybarsacar.sfgpetclinic.model.Owner;
+import dev.aybarsacar.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements CrudService<Owner, Long>
+{
+  @Override
+  public Set<Owner> findAll()
+  {
+    return super.findAll();
+  }
+
+  @Override
+  public Owner findById(Long id)
+  {
+    return super.findById(id);
+  }
+
+  @Override
+  public Owner save(Owner owner)
+  {
+    return super.save(owner.getId(), owner);
+  }
+
+  @Override
+  public void delete(Owner owner)
+  {
+    super.delete(owner);
+  }
+
+  @Override
+  public void deleteById(Long id)
+  {
+    super.deleteById(id);
+  }
+}
